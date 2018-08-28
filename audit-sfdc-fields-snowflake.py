c454d4a28fc1e32e0dcfad3cc23076aa3ee1f038
Jenna Kertz <jk@Jennas-MacBook-Pro.local>
2018-08-28 15:47:21 -0700

    fixed things

--- audit-sfdc-fields-snowflake.py
+++ audit-sfdc-fields-snowflake.py
@@ -26,7 +26,7 @@ df
 table_def = cur.execute('desc table {} type = columns'.format(table_path)).fetchall()
 dw_field_names = [x[0] for x in table_def]
 
-df = pd.DataFrame(column_names)
+df = pd.DataFrame(dw_field_names)
 df
 
 # replace "Opportunity" with relevant object name from SFDC
@@ -43,7 +43,7 @@ df
 
 field_diff = list(set([x[0] for x in sf_field_names]) - set(dw_field_names))
 
-field_diff_proper = [x[1] for x in sf_field_names if x[0] in set(field_diff)]
+field_diff_proper = [x[1] for x in sf_field_names if x[0] in field_diff]
 
 if len(field_diff_proper) > 0:
     logger.info('The following fields exist in SFDC but do not exist in your warehouse. Checking whether these fields contain data to replicate. ' + str(field_diff_proper))
@@ -61,12 +61,11 @@ for field in field_diff_proper:
     
     sf_query = "SELECT Id, {} FROM Opportunity".format(field)
     results = sf.query(sf_query)['records']
+
+    missing_results = [x for x in results if x[field] is not None]
     
-    if len([x[field] for x in results if x[field] is not None]) > 0:
-        missing_field_report.append(field)
-        records = []
-        record_ids = [x['Id'] for x in results if x[field] is not None]
-        records.append(record_ids)
+    if len(missing_results) > 0:
+        record_ids = [x['Id'] for x in missing_results]
         missing_field_report.append({'field':field, 'records':record_ids})
     else:
         logger.info('Field `{}` does not contain any data to replicate.'.format(field))
