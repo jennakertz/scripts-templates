c1eead6a49d023211cc851839c6806d31178262e
Jenna Kertz <jk@Jennas-MacBook-Pro.local>
2018-08-29 10:35:50 -0700

    updated to use new bookmark functions

--- send-data-from-postgres-to-sfdc.py
+++ send-data-from-postgres-to-sfdc.py
@@ -10,38 +10,36 @@ logger = logging.getLogger()
 conn = connections['Default Warehouse']['client']
 cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
-# GET STATE
+# GET BOOKMARKS
 
-# this is the table path where you're saving state for this script
-# the default is "_sdc_script_state"
-state_schema = "_sdc_script_state"
+# this is the table path where you're saving bookmark for this script
+# default is "_sdc_script_bookmarks"
+bookmark_schema = "_sdc_script_bookmarks"
 
-# this is the unique name you used to create the script's state table
-# it is used construct the state table name
+# this is the unique name you used to create the script's bookmark table
+# it is used construct the bookmark table name
 script_name = "salesforce_dev"
-state_table_path = "{}._sdc_{}_state".format(state_schema, script_name)
+bookmark_table_path = "{}._sdc_{}_bookmarks".format(bookmark_schema, script_name)
 
-# this is a list of tables for which you need to get state
-# you may need to save state for multiple tables
-table_states = '(table_1, table_2, table_3)'
+# this is a list of tables for which you need to get bookmark
+# you may need to save bookmarks for multiple tables wihtin a single script
+tables_in_script = "('table_1', 'table_2', 'table_3')"
 
-# grabs the max saved state for each table
-# and saves it in a variable called state
+# GET STORED BOOKMARKS
+# grabs the max saved bookmark for each table
+# and saves it in a variable called bookmarks
 
-state_query = '''
-select
-table,
-max(state) as state
-from
-{}
-where table_name in {}
-group by 1
-'''.format(state_table_path, table_states)
+def get_bookmarks(bookmark_table_path, tables_in_script):
+    bookmark_query = "select schema_name, table_name, max(bookmark) as bookmark from {} where table_name in {} group by 1,2".format(bookmark_table_path, tables_in_script)
 
-cur.execute(state_query)
-results = cur.fetchall()
+    cur.execute(bookmark_query)
+    results = cur.fetchall()
 
-state = results
+    bookmarks = results
+    return bookmarks
+
+
+bookmarks = get_bookmarks(bookmark_table_path, tables_in_script)
 
 # GET NEW DATA TO SEND TO SFDC
 
@@ -49,49 +47,49 @@ state = results
 # and create contacts for them in salesforce
 
 # this is the table we need to reference in our warehouse
-reference_table_path = 'table_1'
-reference_table_state = [x['state'] for x in state if x['table'] == '{}'.format(reference_table_path)]
+reference_table = 'table_1'
+reference_schema = 'schema'
+reference_table_state = [x['bookmark'] for x in bookmarks if x['table_name'] == '{}'.format(reference_table) and x['schema_name'] = '{}'.format(reference_schema)]
 
 get_data = '''
 select
 email, 
 last_name
-from {}
+from {}.{}
 where _sdc_batched_at > {}
-'''.format(reference_table_path, reference_table_state)
+'''.format(reference_schema, reference_table, reference_table_state)
 
-# RETRIEVE NEW STATE
+def write_bookmark(bookmark_table_path, reference_schema, reference_table):
+    # get new bookmark
 
-new_state = '''
-select 
-max(_sdc_batched_at) as new_state
-from {}
-'''.(reference_table_path)
+    new_bookmark = "select max(_sdc_batched_at) as new_bookmark from {}.{}".format(reference_schema, reference_table)
+
+    cur.execute(new_bookmark)
+    new_bookmark = cur.fetchall()
+
+    reference_table_bookmark_new = new_bookmark[0]['new_bookmark']
+
+    # write new bookmark
+    insert_bookmark = '''
+    insert into {}(created_at, schema_name, table_name, bookmark) values (default,'{}','{}','{}')
+    '''.format(bookmark_table_path, reference_schema, reference_table, reference_table_bookmark_new)
+
+    cur.execute(insert_bookmark)
+    conn.commit()
+
+    return
 
 try: 
+
     cur.execute(get_data)
     data = cur.fetchall()
 
-    cur.execute(new_state)
-    new_state = cur.fetchall()
-
-    reference_table_state_new = new_state[0]['new_state']
-    
-    # write new state
-    insert_state = '''
-    insert into {}(created_at, table_name, state) values (default,'{}',{})
-    '''.format(state_table_path, reference_table_path, reference_table_state_new)
-
-    cur.execute(insert_state)
-    
-    conn.commit()
+    write_bookmark(bookmark_table_path, reference_schema, reference_table)
     
 except Exception as e:
     
     logger.error('Closing connection due to error: ' + str(e))
     conn.close()
-    
-pd.DataFrame(data)
 
 # CREATE NEW CONTACTS IN SFDC BASED ON THE DATA FROM THE WAREHOUSE
 
